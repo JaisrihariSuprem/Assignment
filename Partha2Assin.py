bb7e1c53b4ec24c180b1bf125f974172e047a77f
JaisrihariSuprem <sarathiblackchain@gmail.com>
2026-02-24 21:30:16 +0530

    Update Partha2Assin.py

--- Partha2Assin.py
+++ Partha2Assin.py
@@ -1,5 +1,6 @@
-a=int(input("Enter the number: "))
-if(a%2!=0):
-    print(a,"is the Odd number")
+a=int(input("Enter the number: "))   *//take a input from the user
+if(a%2!=0):                            // condition of checking the input to decide the compiler in even or odd
+    print(a,"is the Odd number")      *// it even it will be odd printed 
 else:
-    print(a,"is the Even number")
+    print(a,"is the Even number")        else block is executed the condition false then automatically it becomes the even
+
